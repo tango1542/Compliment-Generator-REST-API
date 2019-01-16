9b6a199847cebd25275ef80c3d047d613556f1a4
vq4988nx <vq4988nx@go.minneapolis.edu>
2019-01-16 14:21:33 -0600

    Trying to upload to heroku

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 import random
+import gunicorn
+
 
 app = Flask(__name__)
 
---
+++ procfile
@@ -0,0 +1,1 @@
+web: gunicorn RestAPI_RandomComliment:app
